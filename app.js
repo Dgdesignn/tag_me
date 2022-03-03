319bdcb71cf98c48760f6addbe06b3588319e11f
Dgdesignn <dg2daielgeraldo@gmail.com>
2022-03-03 07:43:16 +0100

    fazendo o primeiro commit do tag me

---
+++ app.js
@@ -0,0 +1,62 @@
+let tags = [];
+let tagContainer = document.querySelector('.tag-container');
+let input =  tagContainer.querySelector('input');
+
+input.addEventListener('keyup',addTags);
+
+function addTags(event){
+    const keyPressedIsEnter = event.key == 'Enter';
+    if(keyPressedIsEnter){
+       //Quando o botão enter for pressionado
+       input.value.split(',').forEach(tag=>{
+           if(tag){
+               tags.push(tag.trim());
+           }
+       })
+
+       upDateTags()
+       input.value = '';
+    }  
+}
+
+function upDateTags(){
+       clearTags();
+       tags.slice().reverse( ).forEach(tag =>{
+           tagContainer.prepend(createTag(tag));
+       }) ;
+}
+
+function createTag(tag){
+    const div = document.createElement('div');
+    div.classList.add('tag');
+
+    const span = document.createElement('span');
+    span.innerHTML = tag;
+    div.append(span);
+
+    const i = document.createElement('i');
+    i.classList.add('close');
+    i.setAttribute('data-item',tag)
+    i.innerText = 'x';
+    i.onclick = removeTag;
+    span.append(i)
+
+    return div;
+
+}
+
+function removeTag(event){
+    const buttonX = event.currentTarget;
+    const id = buttonX.dataset.item;
+    const index = tags.indexOf(id);
+    tags.splice(index,1);
+    upDateTags()
+}
+
+function clearTags(){
+    tagContainer
+        .querySelectorAll('.tag')
+        .forEach(tagElement=> tagElement.remove())
+}
+
+
---
+++ tex.md
@@ -0,0 +1,12 @@
+#Tag me
+>É uma ideia que tive para implementar as palavras reservadas igual as da redes sociais
+
+`Neste projecto usei um input, quando uma palavra é digita e depois pressionar ENTER a palavra se torna numa Tag onde essa mesma Tag pode se remover`
+
+[Clique aqui para ver como funciona]()
+
+#### Me ajude a melhor esta idia:
+- O que achaste da implementação?
+- O que se pode melhora ainda neste projecto?
+- Se tens como melhor contribuido (código ou ideias), firei muito grato
+
